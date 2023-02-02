aff8a5d21a713a32a3f21d172d75f90ce0923187
StevenEduardoReyesLeon <stevenrl325@gmail.com>
2023-02-01 21:09:12 -0500

    Solucion Cuatro

---
+++ todo_Sobre_Variables_01/02/2023/main.js
@@ -0,0 +1,10 @@
+
+let apellido = "Leon";
+
+let edad = 23;
+
+let estudia = true;
+
+console.log(`Mi apellido es ${apellido}`);
+console.log(`Tengo ${edad} años`);
+console.log(`Estudio actualmente programacion: ${estudia}`);
